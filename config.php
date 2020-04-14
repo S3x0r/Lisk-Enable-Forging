8186bfdc7dba76cbfc851e43475c5864fc33c011
sexor <olisek@gmail.com>
2020-04-15 00:30:51 +0200

    Update

--- config.php
+++ config.php
@@ -1,8 +1,21 @@
 <?php
 
-$PUBLICKEY = ''; /* <----- your publicKey */
-$PASSWORD = '';  /* <----- your password */
+// Delegate Publickey
+$PUBLICKEY = '';
 
-/* optional */
+// Password
+$PASSWORD = '';
+
+// Node IP address
 $NODE_IP   = '127.0.0.1';
-$NODE_PORT = '7000'; /* ports: 5000 - betanet, 7000 - testnet, 8000 - mainnet */
+
+/* ports:
+   5000 - betanet,
+   7000 - testnet,
+   8000 - mainnet  */
+
+// Node Port
+$NODE_PORT = '8000';
+
+// http or https
+$NODE_HTT = 'http';
